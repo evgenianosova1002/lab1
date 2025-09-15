ee7cef72c65855a2828df154b76b1c68f53f5f07
rrlyez <evgenia.noasova1002@gmail.com>
2025-09-15 21:38:58 +0300

    skeleton of the board

--- ConsoleApplication1/board.h
+++ ConsoleApplication1/board.h
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <vector>
+#include <random>
+
+class Board {
+private:
+    int board_size;
+    int sell_selection;
+\
+    std::vector<std::vector<int>> frequencies;
+
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> dist;
+
+public:
+    Board(int board_size, int sell_selection);
+
+    void runExperiment();
+
+    double mean_multiplicity() const;
+
+    double median_multiplicity() const;
+};
