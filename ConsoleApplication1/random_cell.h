d0e392f5898c8f594615dda763fc1a7669e915db
Anna Kucherova <kycherova07@gmail.com>
2025-09-25 19:22:58 +0300

    Added RandomCell class for generating random board positions

--- ConsoleApplication1/random_cell.h
+++ ConsoleApplication1/random_cell.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <random>
+#include <utility>
 
-//std::mt19937 gen;
-//std::uniform_int_distribution<int> dist;
+class RandomCell {
+    int board_size;
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> dist;
+public:
+    explicit RandomCell(int board_size) : board_size(board_size), gen(std::random_device{}()), dist(0, board_size - 1) {}
+    std::pair<int, int> operator()() {
+        return {dist(gen), dist(gen)};
+    }
+};
