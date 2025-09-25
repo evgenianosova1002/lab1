8f040821cf0c64960a65aed530c56bcf3861e358
Anna Kucherova <kycherova07@gmail.com>
2025-09-25 20:31:15 +0300

    readability

--- ConsoleApplication1/random_cell.h
+++ ConsoleApplication1/random_cell.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <random>
 #include <utility>
 
@@ -7,7 +8,11 @@ class RandomCell {
     std::mt19937 gen;
     std::uniform_int_distribution<int> dist;
 public:
-    explicit RandomCell(int board_size) : board_size(board_size), gen(std::random_device{}()), dist(0, board_size - 1) {}
+    explicit RandomCell(int board_size)
+  : board_size(board_size),
+    gen(std::random_device{}()),
+    dist(0, board_size - 1) {}
+
     std::pair<int, int> operator()() {
         return {dist(gen), dist(gen)};
     }
