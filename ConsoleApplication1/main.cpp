9c67b126fe34e01c4a3787d87a2a84e0e20a583c
Anna Kucherova <kycherova07@gmail.com>
2025-09-25 21:01:18 +0300

    main

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,10 +1,18 @@
 ﻿#include "board.h"
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "hello" << endl;
+	int board_size = 0;
+	int sell_selection =0;
+
+	cout << "Enter n (board size): ";
+	cout << "Enter t (number of cell selections): ";
+
 	return 0;
 }
