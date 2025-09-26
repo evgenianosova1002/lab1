3bbf6d8f4e13371b88448840f27ecdd7d5e5262b
Anna Kucherova <kycherova07@gmail.com>
2025-09-26 09:23:04 +0300

    input validation added

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -12,7 +12,19 @@ int main()
 	int cell_selection_count = 0;
 
 	cout << "Enter n (board size): ";
+	cin >> board_size;
+
 	cout << "Enter t (number of cell selections): ";
+	cin >> cell_selection_count;
+
+	if (board_size <= 0) {
+		cerr << "Error: n must be > 0.\n";
+		return 1;
+	}
+	if (cell_selection_count < 0) {
+		cerr << "Error: t must be >= 0.\n";
+		return 1;
+	}
 
 	return 0;
 }
--- ConsoleApplication1/random_cell.h
+++ ConsoleApplication1/random_cell.h
@@ -9,9 +9,9 @@ class RandomCell {
     std::uniform_int_distribution<int> dist;
 public:
     explicit RandomCell(int board_size)
-  : board_size(board_size),
-    gen(std::random_device{}()),
-    dist(0, board_size - 1) {}
+        : board_size(board_size),
+          gen(std::random_device{}()),
+          dist(0, board_size - 1) {}
 
     std::pair<int, int> operator()() {
         return {dist(gen), dist(gen)};
